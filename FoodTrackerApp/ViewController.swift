8dbc99f7d997523b02019ed82eaa5e8880d11210
Ezequiel <ezequiel3434@gmail.com>
2020-07-21 21:59:57 -0300

    Initial Commit

---
+++ FoodTrackerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTrackerApp
+//
+//  Created by Ezequiel Parada Beltran on 21/07/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
